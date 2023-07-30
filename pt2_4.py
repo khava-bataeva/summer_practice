d2472526267d3f078f3488d041ac30de82911318
khava-bataeva <138183413+khava-bataeva@users.noreply.github.com>
2023-07-30 11:31:31 +0300

    Change by PEP8

--- pt2_4.py
+++ pt2_4.py
@@ -1,16 +1,15 @@
 import math
 
-a = int(input("Введите значение a= "))
-b = int(input("Введите значение b= "))
-c = int(input("Введите значение c= "))
-D = b ** 2 - 4 * a * c
-if D < 0:
-  print("Корней нет")
-elif D == 0:
-  x = -b / 2 * a
-  print ('один корень: ', x)
-else:
-  x1 = (-b + math.sqrt(D)) / (2 * a)
-  x2 = (-b - math.sqrt(D)) / (2 * a)
-  print('первый корень: ', x1, 'второй корень:', x2)
- 
+a = int(input())
+b = int(input())
+c = int(input())
+d = b ** 2 - 4 * a * c
+if d > 0:
+    x1 = (-b - math.sqrt(d)) / (2 * a)
+    x2 = (-b + math.sqrt(d)) / (2 * a)
+    print('x1=', x1, 'x2=', x2)
+if d == 0:
+    x = -b / (2 * a)
+    print('x=', x)
+if d < 0:
+    print('Нет корней')
