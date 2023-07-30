483afed90e3fa38f23428036a6ff5822337f8128
khava-bataeva <138183413+khava-bataeva@users.noreply.github.com>
2023-07-30 09:13:01 +0300

    correction of errors in the 3rd task of the 1st block
    The program compares three numbers, outputs the maximum and all numbers in descending order.

--- pt1_3.py
+++ pt1_3.py
@@ -1,13 +1,22 @@
-a = int(input('Введите число a -'))
-b = int(input('Введите число b -'))
-c = int(input('Введите число c -'))
+a = int(input('Введите число a '))
+b = int(input('Введите число b '))
+c = int(input('Введите число c '))
 print('Максимальным числом является: ', end='')
-if b <= a >= c: print(a)
-elif a <= b >= c: print(b)
-elif a <= c >= b: print(c)
-if (b <= a >= c and b>c): print(a, b, c)
-elif (b <= a >= c and c>b): print(a, c, b)
-elif (a <= b >= c and a>c): print(b, a, c)
-elif (a <= b >= c and c>a): print(b, c, a)
-elif (a <= c >= b and a>b): print(c, a, b) 
-elif (a <= c >= b and b>a): print(c, b, a) 
+if b <= a >= c:
+    print(a)
+elif a <= b >= c:
+    print(b)
+elif a <= c >= b:
+    print(c)
+if (b <= a >= c and b > c):
+    print(a, b, c)
+elif (b <= a >= c and c > b):
+    print(a, c, b)
+elif (a <= b >= c and a > c):
+    print(b, a, c)
+elif (a <= b >= c and c > a):
+    print(b, c, a)
+elif (a <= c >= b and a > b):
+    print(c, a, b)
+elif (a <= c >= b and b > a):
+    print(c, b, a)
