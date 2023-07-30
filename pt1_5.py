3564e27cb57953673d879eda5b1f4b906bb52a3b
khava-bataeva <138183413+khava-bataeva@users.noreply.github.com>
2023-07-30 09:43:21 +0300

    program 5 from block 1
    I wrote a program that solves the following problem: In the soccer championship the team is given 3 points for winning,  0 points for losing, and 0 points for a draw. - 1. The number of points received by the team for the game is known. Determined the verbal the result of the game (win, lose or draw).  The number is entered from the console.

--- pt1_5.py
+++ pt1_5.py
@@ -1,4 +1,7 @@
 k = int(input('Сколько команда набрала очков? '))
-if k == 3: print('Выйгрыш')
-elif k == 1: print('Ничья')
-elif k == 0: print('Проигрыш')
+if k == 3:
+    print('Выйгрыш')
+elif k == 1:
+    print('Ничья')
+elif k == 0:
+    print('Проигрыш')
