e320bb049201ee767c6696d0c0fb95c2030b6750
khava-bataeva <138183413+khava-bataeva@users.noreply.github.com>
2023-07-30 12:34:25 +0300

    correction of errors E225 and E231
    added a space around the operator and after the comma

--- pt2_11.py
+++ pt2_11.py
@@ -1,8 +1,8 @@
-print("Введите наименьшее число диапазона")
-a=int(input())
+print('Введите наименьшее число диапазона')
+a = int(input())
 print('введите наибольшее число диапазона')
-b=int(input())
+b = int(input())
 sum_ = 0
-for i in range(a,b + 1):
+for i in range(a, b + 1):
     sum_ += i
 print(sum_)
