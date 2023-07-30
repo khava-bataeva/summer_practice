46098b340d681290747bbb35daed54122370a282
khava-bataeva <138183413+khava-bataeva@users.noreply.github.com>
2023-07-30 11:14:00 +0300

    Task 3 from block 2 in the form .py file
    fixed bug E225, added a space around the operator

--- pt2_3.py
+++ pt2_3.py
@@ -3,10 +3,10 @@ def is_armstrong(num):
     n = len(num_str)
     sum = 0
     for digit in num_str:
-        sum += int(digit)**n
+        sum += int(digit) ** n
     if sum == num:
         return True
     else:
         return False
-num=int(input())
+num = int(input())
 print(is_armstrong(num))
