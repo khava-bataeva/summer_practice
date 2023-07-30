a7f31d9103abb41c5eff63a2106d378ec4d383be
khava-bataeva <138183413+khava-bataeva@users.noreply.github.com>
2023-07-30 14:22:28 +0300

    block 3 in the form .py file

--- pt4_2.py
+++ pt4_2.py
@@ -3,12 +3,16 @@ def direction(s, first, second, palindromes):
         palindromes.add(s[first: second + 1])
         first = first - 1
         second = second + 1
+        
+
 def findSubstrings(s):
     palindromes = set()
     for i in range(len(s)):
         direction(s, i, i, palindromes)
         direction(s, i, i + 1, palindromes)
-    print(palindromes, end='')
+    print(palindromes, end = '')
+
+
 if __name__ == '__main__':
     s = 'aabbdaad'
     findSubstrings(s)
