033feedb3465990787ead1de8b019b672363e1d2
khava-bataeva <138183413+khava-bataeva@users.noreply.github.com>
2023-07-30 14:08:27 +0300

    Change by PEP8

--- pt3_10.py
+++ pt3_10.py
@@ -1,9 +1,9 @@
 def count(str):
     count_letter = {letter: str.count(letter) for letter in str if letter != ' '}
     return count_letter
+
+
 str = input('Введите строку: ')
 res = count(str)
-
-
 print('Словарь, содержащий буквы и их количество:')
 print(res)
