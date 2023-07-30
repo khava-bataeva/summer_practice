069b6f0e1e4fb12186058bc6e729908bd5d80504
khava-bataeva <138183413+khava-bataeva@users.noreply.github.com>
2023-07-30 12:10:11 +0300

    Change by PEP8

--- pt2_9.py
+++ pt2_9.py
@@ -1,10 +1,8 @@
-n = int(input("Введите число: "))
-def find_max(n):
-    n_str = str(n)
-    max_n = max(n_str)
-    position1 = n_str[::-1].index(max_n) + 1
-    position2 = n_str.index(max_n) + 1
-    return position1, position2
-result1, result2 = find_max(n)
-print("Порядковый номер максимальной цифры (считая от конца числа):", result1)
-print("Порядковый номер максимальной цифры (считая от начала числа):", result2)
+a = int(input('Введите число, в котором все цифры различны: '))
+a = str(a)
+arr = []
+for i in a:
+    arr.append(i)
+print(arr.index(max(arr)) + 1)
+arr.reverse()
+print(arr.index(max(arr)) + 1)
